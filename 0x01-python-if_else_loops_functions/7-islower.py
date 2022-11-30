6edac32f256abf4d53d1966eca5edad769584e83
ndegz <ndegz@example.com>
2022-11-30 02:43:27 -0800

    loops

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,7 @@
+def islower(c):
+    # loop c
+    for i in c:
+        if 97 <= ord(i) <= 123:
+            return True
+        else:
+            return False
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+def uppercase(str):
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 123:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
+    print("")
