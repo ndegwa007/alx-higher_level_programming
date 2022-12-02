75b24d158e10dd4bb38ef944e697883a0ae9c113
ndegz <ndegz@example.com>
2022-12-01 23:23:10 -0800

    module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-    if argv[1] == "":
+    if len(argv) == 1:
         print("0 arguments.")
     elif len(argv) == 2:
         print("1 argument:")
