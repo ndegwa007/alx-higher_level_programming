e4fb7e6d527d67a918068c35cc2d4d4f53e7b94b
ndegz <ndegz@example.com>
2022-12-06 09:44:56 -0800

    data structs

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == "":
-        t[0] == None
     t = tuple(sentence)
+    if sentence == "":
+        t[0] = None
     return len(t), t[0]
