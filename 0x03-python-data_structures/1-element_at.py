b76d836c0a1c66550e927e7c2d9db098bd08fc78
ndegz <ndegz@example.com>
2022-12-06 07:10:13 -0800

    data structures

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        print("None")
-    if idx >= len(my_list):
-        print("None")
+    if idx < 0 or idx > len(my_list) - 1:
+        return None
     return my_list[idx]
