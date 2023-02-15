dda9fcb7d7429b536b8523388d0bc292457cd0c5
ndegz <ndegz@example.com>
2023-02-15 04:58:14 -0800

    sql

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,2 @@
+-- computing the average score
+SELECT AVG(score) AS average FROM `second_table`;
--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,2 @@
 -- script that displays the number of records with id=89
-SELECT COUNT(*) AS count
-FROM hbtn_0c_0.first_table
-WHERE id = 89;
+SELECT COUNT(*) FROM `first_table` WHERE id = 89;
