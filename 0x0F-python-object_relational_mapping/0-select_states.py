ce04a7958ae87a45f691e266f4928e9735f37552
ndegz <ndegwa8781@gmail.com>
2023-03-22 12:01:54 +0300

    relational mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-This module lists all states from the database hbtn_0e_0_usa
-"""
+# This module lists all states from the database hbtn_0e_0_usa
 import MySQLdb
 import sys
 
@@ -11,19 +9,22 @@ mysql_password = sys.argv[2]
 mysql_db = sys.argv[3]
 
 if __name__ == '__main__':
-    #connect to the mysql server
-    db = MySQLdb.connect(host="localhost",user=mysql_user,passwd=mysql_password,db=mysql_db)
+    # connect to the mysql server
+    db = MySQLdb.connect(
+            host="localhost",
+            user=mysql_user, passwd=mysql_password, db=mysql_db)
+
     cursor = db.cursor()
     # Execute the SQL query to fetch all states from the database
     query = "SELECT * FROM states ORDER BY id ASC"
     cursor.execute(query)
 
-    #fetch all the rows returned by the query
+    # fetch all the rows returned by the query
     rows = cursor.fetchall()
 
     for row in rows:
         print(row)
 
-    #close the cursor and database connections
+    # close the cursor and database connections
     cursor.close()
     db.close()
