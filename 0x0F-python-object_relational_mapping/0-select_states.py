5a67a5e1ab0da62f5f3195729a8d5786f17a70bf
ndegz <ndegwa8781@gmail.com>
2023-03-22 12:16:56 +0300

    relational mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# This module lists all states from the database hbtn_0e_0_usa
+""" This module lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
