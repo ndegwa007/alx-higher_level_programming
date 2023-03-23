5c4a47c4a8f386000ff3d76e80af3790c33d2a71
ndegz <ndegwa8781@gmail.com>
2023-03-23 20:20:44 +0300

    first state object

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -20,6 +20,6 @@ if __name__ == '__main__':
         if q is not None:
             print("{}: {}".format(q.id, q.name))
         else:
-            print("Nothing" + "\n")
+            print("" + "\n")
 
     session.close()
