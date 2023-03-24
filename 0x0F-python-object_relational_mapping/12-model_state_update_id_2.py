76cc456c4f29666882b889761b3e64fa39e3fc8e
ndegz <ndegwa8781@gmail.com>
2023-03-24 17:40:45 +0300

    update database

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" module updates State object
+from the database"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+if __name__ == "__main__":
+    username, password, database = argv[1:]
+
+    engine = create_engine(
+            "mysql+mysqldb://{}:{}@localhost/{}".format(
+                username, password, database))
+
+    Session = sessionmaker(bind=engine)
+
+    with Session() as session:
+        state = session.query(State).filter_by(id=2).first()
+        state.name="New Mexico"
+        session.commit()
