02a9c124bbe49c85817b078722a8c0546f06acb9
ndegz <ndegwa8781@gmail.com>
2023-05-03 14:28:56 +0300

    body of a 200 status code response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a URL and returns the body of the response for a 200 status code
-curl -s -o /dev/null -w "%{http_code}\n%{body}" -L $1 | awk 'BEGIN {RS="\n\n|\r\n\r\n"} ; {if ($1 == "200") print $2}'
+#script that sends a GET request to a URL and displays only the body of the response with a 200 status code
+curl -s -w "\nHTTP status code: %{http_code}\n" | sed -n '/^HTTP\/1.1 200 /, $p'
