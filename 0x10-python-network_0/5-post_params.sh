b2743f07073aacc90f9d439d12a3eeed6b88a25b
ndegz <ndegwa8781@gmail.com>
2023-05-03 19:16:39 +0300

    more on curl

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -s $1 -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
+# bash script that sends a post request to a passed url
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
