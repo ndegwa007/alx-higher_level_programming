97dcd3df51ef9aa8318085b77687067a3e3c2485
ndegz <ndegwa8781@gmail.com>
2023-05-23 15:03:55 +0300

    read file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
 // script reads and prints contents of a file
-
 import { readFile } from 'node:fs';
+
 const file = process.argv[2];
 readFile(file, 'utf-8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
 });
