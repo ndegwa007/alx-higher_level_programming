04e3908bb34edb77926c71122d8e7c6fe5ae2d22
ndegz <ndegwa8781@gmail.com>
2023-05-23 19:26:06 +0300

    status using request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// script that displays the status code of a GET request
+
+const request = require('request');
+
+const req_url = process.argv[2];
+request(req_url, function (err, response) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('code: ', response.statusCode);
+  }
+});
