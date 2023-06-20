d3ec585f96b645398a27dab280ca7b5c9ba50715
ndegz <ndegwa8781@gmail.com>
2023-06-20 21:36:09 +0300

    peak value

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -20,8 +20,8 @@ def find_peak(list_of_integers: [int]) -> int:
     while (low <= high):
         mid = (low + high) // 2
 
-        if list_of_integers[mid] > list_of_integers[mid + 1] and
-        list_of_integers[mid] > list_of_integers[mid - 1]:
+        if list_of_integers[mid] > list_of_integers[mid + 1] and \
+                list_of_integers[mid] > list_of_integers[mid - 1]:
             return list_of_integers[mid]
 
         if list_of_integers[mid] < list_of_integers[mid + 1]:
